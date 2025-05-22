712a65e06f6a5c6be1fce1113564249c5f894cd4
jinsik <hoppure@naver.com>
2025-05-22 16:54:25 +0900

    feat formatting

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,22 +3,25 @@ import random
 
 import numpy as np
 
+
 def init_seed():
     np.random.seed(0)
     random.seed(0)
 
+
 def project_path():
     return os.path.join(
-        os.path.dirname( # /opt/mlops/src/tuils
-            os.path.abspath(__file__) # /opt/mlops/src/utils/utils.py
+        os.path.dirname(  # /opt/mlops/src/tuils
+            os.path.abspath(__file__)  # /opt/mlops/src/utils/utils.py
         ),
-        "..", # /opt/mlops/src
+        "..",  # /opt/mlops/src
         ".."  # /opt/mlops  --> project_path
     )
 
+
 def model_dir(model_name):
     return os.path.join(
-        project_path(), # /opt/mlops
-        "models",       # /opt/mlops/models   
-        model_name      # /opt/mlops/models/model_name 
+        project_path(),  # /opt/mlops
+        "models",       # /opt/mlops/models
+        model_name      # /opt/mlops/models/model_name
     )
